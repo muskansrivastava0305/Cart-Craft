84f2e039e0ac9ae0ee3d081c78936c4a9ad8f7df
Muskan Kumari <muskansrivastava0305@gmail.com>
2024-09-04 00:01:29 +0530

    creating App

--- src/component/Container.js
+++ src/component/Container.js
@@ -2,13 +2,14 @@ import {connect} from 'react-redux'
 import App from '../App'
 import {addToCart} from  '../component/action'
 import cardItem from './reducer'
+import CartOrder from './CartOrder'
 
 
 const stateProps=state =>({
-   cardItem: state.cart.items,
+
 });
 const dispatchProps = dispatch => ({
     addToCartHandler:data => dispatch(addToCart(data))
 })
 
-export default connect(stateProps, dispatchProps )(App)
+export default connect(stateProps, dispatchProps )(CartOrder)
